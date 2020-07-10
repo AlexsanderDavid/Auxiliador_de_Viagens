e74a18ee416cbe4610ea78540e204b4e56a564d5
Alexsander David Manikoski Costa <68086828+AlexsanderDavid@users.noreply.github.com>
2020-07-09 21:00:14 -0300

    Add files via upload

---
+++ AdV.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL,"portuguese");
+    float value, kmaut, km, kmh, aut, l, gas = 3.6, et = 2.79, ds = 3.27;
+    int type, h, a;
+
+        cout<<"Bem vindo ao auxiliador de viagens!"<<endl;
+        cout<<"Favor Inserir a distância a ser percorrida (km): ";
+        cin>>km;
+        cout<<endl;
+        cout<<"Favor Inserir a velocidade média do automóvel (km/h) : ";
+        cin>>kmh;
+        cout<<endl;
+        cout<<"Favor Selecionar um dos combustíveis abaixo: ";
+        cout<<endl;
+        cout<<endl;
+        cout<<"         Gasolina = 1"<<endl;
+        cout<<"          Etanol = 2"<<endl;
+        cout<<"          Diesel = 3"<<endl;
+        cout<<endl;
+        cout<<"               ";
+        cin>>type;
+        cout<<endl;
+        cout<<"Favor Inserir a autonomia média esperada para o combustível selecionado (km/l): ";
+        cin>>aut;
+        cout<<endl;
+
+        kmaut=(km/aut);
+        h=(km/kmh);
+
+        cout<<"Serão necessários ao menos "<<kmaut<<" Litros para concluir a viagem.";
+        cout<<endl;
+        cout<<endl;
+        cout<<"Favor Inserir a quantidade em litros que será abastecido: ";
+        cin>>l;
+
+        switch (type) {
+
+    case 1:
+        value=(l*gas);
+        break;
+    case 2:
+        value=(l*et);
+        break;
+    case 3:
+        value=(l*ds);
+        break;
+    default:
+        cout<<endl;
+        cout<<"Favor Inserir um valor válido"<<endl;
+        break;
+}
+
+    cout<<endl;
+    cout<<"O valor gasto em combustível será de R$"<<value<<" e a viagem levará dentre "<<h<<" e "<<h+1<<" horas."<<endl;
+        return 0;
+}
